04a45f905949bea595fe6eaf899e8261d9178d92
xuefenhong <979671647@qq.com>
2019-01-24 10:49:15 +0800

    NavigationBar的实现和使用

--- js/page/MyPage.js
+++ js/page/MyPage.js
@@ -1,42 +1,94 @@
 import React, {Component} from 'react';
-import {Button, StyleSheet, Text, View} from 'react-native';
+import {Button, StyleSheet, Text, View, TouchableOpacity} from 'react-native';
 import NavigationUtil from "../navigator/NavigationUtil";
+import NavigationBar from '../common/NavigationBar'
+import Ionicons from 'react-native-vector-icons/Ionicons'
+import Feather from 'react-native-vector-icons/Feather'
+const THEME_COLOR = '#678';
 type Props = {};
 export default class MyPage extends Component<Props> {
-  render() {
-    const {navigation} = this.props;
+  getRightButton() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to MyPage!</Text>
-        <Text onPress={() => {
-          NavigationUtil.goPage({
-            navigation: this.props.navigation
-          }, 'DetailPage')
-        }}>跳转到详情页</Text>
-        <Button
-          title={'Fetch 使用'}
-          onPress={() => {
-            NavigationUtil.goPage({
-              navigation: this.props.navigation
-            }, 'FetchDemoPage')
-          }}/>
-        <Button
-          title={'AsyncStorage 使用'}
-          onPress={() => {
-            NavigationUtil.goPage({
-              navigation: this.props.navigation
-            }, 'AsyncStorageDemoPage')
-          }}/>
-        <Button
-          title={'DataStore 使用'}
-          onPress={() => {
-            NavigationUtil.goPage({
-              navigation: this.props.navigation
-            }, 'DataStoreDemoPage')
-          }}/>
+      <View style={{flexDirection: 'row', }}>
+        <TouchableOpacity onPress={() => {}}>
+          <View style={{padding: 5, marginRight: 8}}>
+            <Feather
+              name={'search'}
+              size={24}
+              style={{color: '#fff'}}
+            />
+          </View>
+        </TouchableOpacity>
       </View>
-    );
+    )
   }
+
+  getLeftButton(callBack) {
+    return (
+      <TouchableOpacity
+        style={{padding: 8, paddingLeft: 12}}
+        onPress={() => {}}
+      >
+        <Ionicons
+          name={'ios-arrow-back'}
+          size={26}
+          style={{color: '#fff'}}
+        />
+
+      </TouchableOpacity>
+    )
+  }
+
+  render() {
+    let statusBar = {
+      backgroundColor: THEME_COLOR,
+      barStyle: 'light-content'
+    };
+    let navigationBar =
+      <NavigationBar
+        title={'我的'}
+        statusBar={statusBar}
+        style={{backgroundColor: THEME_COLOR}}
+        rightButton={this.getRightButton()}
+        leftButton={this.getLeftButton()}
+      />;
+    return (
+      <View style={{flex: 1}}>
+        {navigationBar}
+        <Text style={styles.welcome}>MyPage</Text>
+        <View style={styles.container}>
+          <Text style={styles.welcome}>Welcome to MyPage!</Text>
+          <Text onPress={() => {
+            NavigationUtil.goPage({
+              navigation: this.props.navigation
+            }, 'DetailPage')
+          }}>跳转到详情页</Text>
+          <Button
+            title={'Fetch 使用'}
+            onPress={() => {
+              NavigationUtil.goPage({
+                navigation: this.props.navigation
+              }, 'FetchDemoPage')
+            }}/>
+          <Button
+            title={'AsyncStorage 使用'}
+            onPress={() => {
+              NavigationUtil.goPage({
+                navigation: this.props.navigation
+              }, 'AsyncStorageDemoPage')
+            }}/>
+          <Button
+            title={'DataStore 使用'}
+            onPress={() => {
+              NavigationUtil.goPage({
+                navigation: this.props.navigation
+              }, 'DataStoreDemoPage')
+            }}/>
+        </View>
+      </View>
+    )
+  }
+
 }
 
 const styles = StyleSheet.create({
--- js/page/PopularPage.js
+++ js/page/PopularPage.js
@@ -6,10 +6,11 @@ import {createMaterialTopTabNavigator, createAppContainer} from 'react-navigatio
 import  NavigationUtil from '../navigator/NavigationUtil'
 import PopularItem from '../common/PopularItem'
 import Toast from 'react-native-easy-toast'
+import NavigationBar from '../common/NavigationBar'
 
 const URL = 'https://api.github.com/search/repositories?q=';
 const QUERY_STR = '&sort=stars';
-const THEME_COLOR = 'red';
+const THEME_COLOR = '#678';
 type Props = {};
 export default class PopularPage extends Component<Props> {
   constructor(props){
@@ -36,6 +37,15 @@ export default class PopularPage extends Component<Props> {
     return tabs
   }
   render() {
+    let statusBar = {
+      backgroundColor: THEME_COLOR,
+      barStyle: 'light-content'
+    };
+    let navigationBar = <NavigationBar
+      title={'最热'}
+      statusBar={statusBar}
+      style={{backgroundColor: THEME_COLOR}}
+    />;
     const TabNavigator = createAppContainer(createMaterialTopTabNavigator(
       this._genTabs(), {
         tabBarOptions: {
@@ -51,7 +61,12 @@ export default class PopularPage extends Component<Props> {
       }
     ))
     return (
-      <TabNavigator/>
+      <View style={{flex: 1}}>
+        {navigationBar}
+        <TabNavigator/>
+
+      </View>
+
     );
   }
 }
@@ -172,6 +187,7 @@ const mapDispatchToProps = dispatch => ({
   onLoadMorePopular: (storeName, pageIndex, pageSize, items, callBack) => dispatch(actions.onLoadMorePopular(storeName, pageIndex, pageSize, items, callBack))
 })
 
+// 注意: connect 只是个function, 并不一定非要放在export后面
 const PopularTabPage = connect(mapStateToProps, mapDispatchToProps)(PopularTab)
 
 const styles = StyleSheet.create({
